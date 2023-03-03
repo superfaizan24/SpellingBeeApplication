9b29ec7bff284d7e72b67f6bb80ad8b808e12c99
Faizan Zameer <87537600+superfaizan24@users.noreply.github.com>
2023-03-03 01:24:02 -0600

    Update difficulty-selection.component.ts

--- SpellingBee.Web/src/app/auth/difficulty-selection/difficulty-selection.component.ts
+++ SpellingBee.Web/src/app/auth/difficulty-selection/difficulty-selection.component.ts
@@ -13,32 +13,6 @@ export class DifficultySelectionComponent {
   
 
    constructor(private SharedService:SharedService){}
-  // selectedImage: string = '';
-  // checkboxes: boolean[] = [false, false, false];
-
-  // toggleCheckbox(index: number): void {
-  //   this.checkboxes[index - 1] = !this.checkboxes[index - 1];
-  // }
-
-  // selectImage(index: number): void {
-  //   this.checkboxes.fill(false);
-  //   this.checkboxes[index - 1] = true;
-  //   this.selectedImage = `Image ${index}`;
-  // }
-
-  // confirmSelection(): void {
-  //   const selectedImageIndex = this.checkboxes.findIndex(checkbox => checkbox === true);
-  //   if (selectedImageIndex >= 0) {
-  //     this.selectedImage = `Image ${selectedImageIndex + 1}`;
-  //   } else {
-  //     this.selectedImage = '';
-  //   }
-  // }
-
-
-  
-
-
 
   confirm() {
     const userPreference = {
