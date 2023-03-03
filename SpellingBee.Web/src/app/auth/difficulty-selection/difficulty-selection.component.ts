760f18ed39b89d94c781ef23cffe4f1c2d7b0b5e
Faizan Zameer <87537600+superfaizan24@users.noreply.github.com>
2023-03-03 01:29:37 -0600

    Add files via upload

---
+++ SpellingBee.Web/src/app/auth/difficulty-selection/difficulty-selection.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-difficulty-selection',
+  templateUrl: './difficulty-selection.component.html',
+  styleUrls: ['./difficulty-selection.component.scss']
+})
+export class DifficultySelectionComponent {
+  selectedDifficulty:number;
+  email=JSON.parse(localStorage.getItem('user'));
+  
+  
+
+   constructor(private SharedService:SharedService){}
+  
+  confirm() {
+    const userPreference = {
+      user: { email: this.email },
+      preference: { difficulty: this.selectedDifficulty }
+    };
+  
+    this.SharedService.selectDifficulty(userPreference).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+  
+
+  
+
+}
