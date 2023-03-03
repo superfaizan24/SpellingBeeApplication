7abd0b3a12dbec1f45867456397ae9a694e0d6a9
Faizan Zameer <87537600+superfaizan24@users.noreply.github.com>
2023-03-03 01:22:55 -0600

    Add files via upload

--- SpellingBee.Web/src/app/auth/login/login.component.ts
+++ SpellingBee.Web/src/app/auth/login/login.component.ts
@@ -7,38 +7,49 @@ import { SharedService } from "src/app/shared.service";
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
+  providers: [SharedService]
 })
 export class LoginComponent implements OnInit {
   public newUser = false;
-  // public user: firebase.User;
   public loginForm: FormGroup;
-  public show: boolean = false
+  public show: boolean = false;
   public errorMessage: any;
+  email: string;
+  password: string;
 
-  constructor(private fb: FormBuilder, public router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    public router: Router,
+    private SharedService: SharedService
+  ) {
     this.loginForm = this.fb.group({
-      email: ["Test@gmail.com", [Validators.required, Validators.email]],
-      password: ["test123", Validators.required],
+      email: [this.email, [Validators.required, Validators.email]],
+      password: [this.password, Validators.required],
     });
   }
 
   ngOnInit() {}
 
-  login() {
-    if (this.loginForm.value["email"] == "Test@gmail.com" && this.loginForm.value["password"] == "test123") {
-      let user = {
-        email: "Test@gmail.com",
-        password: "test123",
-        name: "test user",
-      };
-      localStorage.setItem("user", JSON.stringify(user));
-      this.router.navigate(["/dashboard/default"]);
-    }
+  onSignIn() {
+    this.SharedService.loginUser(this.email, this.password).subscribe((res) => {
+      if (res) {
+        // Store user data and token in local storage
+        localStorage.setItem('user', JSON.stringify(this.email));
+        localStorage.setItem("isLoggedIn", "true");
+
+        // Redirect user to home screen
+        this.router.navigate(["/src/app/components/dashboard/default"]);
+      } else {
+        alert("Login was invalid");
+      }
+    });
   }
 
-
-
-  showPassword(){
-    this.show = !this.show
+  showPassword() {
+    this.show = !this.show;
   }
+
+  
+
+  
 }
