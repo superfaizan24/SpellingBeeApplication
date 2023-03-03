35def94d2ada44022fce4d0bb829ad1d7879a674
Faizan Zameer <87537600+superfaizan24@users.noreply.github.com>
2023-03-03 01:34:32 -0600

    Update auth.routing.module.ts

--- SpellingBee.Web/src/app/auth/auth.routing.module.ts
+++ SpellingBee.Web/src/app/auth/auth.routing.module.ts
@@ -13,4 +13,7 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthenticationRoutingModule { }
+export class AuthenticationRoutingModule { } /* Error occurs on this line; this is the error message even though my Experimental decorators is set to true in tsconfig: 
+"Experimental support for decorators is a feature that is subject to change in a future release. 
+Set the 'experimentalDecorators' option in your 'tsconfig' or 'jsconfig' to remove this warning."
+*/
